ea40a81383da8e79c09d3b8a538ebc0454b00beb
Jorge Christino <jorgecsantofer@gmail.com>
2023-02-02 13:23:19 -0300

    Project created

---
+++ src/main/java/com/projeto_web/projetowebjava/ProjetoWebJavaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_web.projetowebjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_web/projetowebjava/ProjetoWebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_web.projetowebjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
