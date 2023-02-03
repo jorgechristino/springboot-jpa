905185a45c4702e9c36cdbe02d8fa91a629989f3
Jorge Christino <jorgecsantofer@gmail.com>
2023-02-03 19:40:49 -0300

    Payment, one-to-one association

--- src/main/java/com/projeto_web/projetowebjava/config/TestConfig.java
+++ src/main/java/com/projeto_web/projetowebjava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projeto_web.projetowebjava.entities.Category;
 import com.projeto_web.projetowebjava.entities.Order;
 import com.projeto_web.projetowebjava.entities.OrderItem;
+import com.projeto_web.projetowebjava.entities.Payment;
 import com.projeto_web.projetowebjava.entities.Product;
 import com.projeto_web.projetowebjava.entities.User;
 import com.projeto_web.projetowebjava.entities.enums.OrderStatus;
@@ -76,7 +77,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
-            orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(0, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        
+        orderRepository.save(o1);
     }
 
 }
---
+++ src/main/java/com/projeto_web/projetowebjava/entities/Payment.java
@@ -0,0 +1,82 @@
+package com.projeto_web.projetowebjava.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(){
+    }
+
+    public Payment(int id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Payment other = (Payment) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+}
